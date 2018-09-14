6384c310615fc7a62528660a867e2f94765433f4
Slyxes <slyxes.s@gmail.com>
2018-09-14 11:23:38 +0200

    MadeHealthPickupDissapearOnPickup

---
+++ PotatoMan/Assets/Scripts/Health_Pickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_Pickup : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log("Collision!");
+        Destroy(gameObject);
+    }
+
+}
--- PotatoMan/Assets/Scripts/PlayerControllerScript.cs
+++ PotatoMan/Assets/Scripts/PlayerControllerScript.cs
@@ -27,7 +27,7 @@ public class PlayerControllerScript : MonoBehaviour {
     private void FixedUpdate() {
         bool wasGrounded = Grounded;
         Grounded = false;
-
+        
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
         // This can be done using layers instead but Sample Assets will not overwrite your project settings.
         Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, GroundedRadius, WhatIsGround);
